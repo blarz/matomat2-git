8999ebb5686d0e049987521f4b1a49751c926a5a
Patrick Himmelmann <himmelmannp@googlemail.com>
2014-08-12 00:23:01 +0200

    Fixed usage of foreign keys

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
 
 from config import dbengine
@@ -22,17 +22,20 @@ class Item(Base):
 class Sale(Base):
 	__tablename__='sales'
 	id=Column(Integer,primary_key=True)
-	user=ForeignKey('users.id')
-	item=ForeignKey('items.id')
+	user_id=Column(Integer,ForeignKey('users.id'))
+	item_id=Column(Integer,ForeignKey('items.id'))
 	amount=Column(Integer)
 	time=Column(DateTime, default=func.now())
+	user=relationship('User')
+	item=relationship('Item')
 
 class Pay(Base):
 	__tablename__='pays'
 	id=Column(Integer,primary_key=True)
-	user=ForeignKey('users.id')
+	user_id=Column(Integer,ForeignKey('users.id'))
 	amount=Column(Integer)
 	time=Column(DateTime, default=func.now())
+	user=relationship('User')
 
 __engine=create_engine(dbengine)
 Base.metadata.create_all(__engine)
