24a36c9e482e54a8f0b3d2135ad05b855732eb47
Patrick Himmelmann <himmelmannp@googlemail.com>
2014-08-24 00:46:24 +0200

    Added money transfer to object interface

--- matomat.py
+++ matomat.py
@@ -1,4 +1,5 @@
 from authentication import check_user, create_user, get_user
+from datetime import datetime
 import database as db
 
 class NotAutheticatedError(Exception):
@@ -38,20 +39,29 @@ class matomat(object):
 	def balance(self):
 		money_in=sum((x.amount for x in self.session.query(db.Pay).filter(db.Pay.user==self._user)))
 		money_out=sum((x.amount for x in self.session.query(db.Sale).filter(db.Sale.user==self._user)))
-		res=money_in-money_out
+		transfers_in=sum((x.amount for x in self.session.query(db.Transfer).filter(db.Transfer.recipient==self._user)))
+		transfers_out=sum((x.amount for x in self.session.query(db.Transfer).filter(db.Transfer.sender==self._user)))
+		res=money_in-money_out+transfers_in-transfers_out
 		return res
 
 	@require_auth
 	def details(self):
 		money_in=self.session.query(db.Pay).filter(db.Pay.user==self._user).all()
 		money_out=self.session.query(db.Sale).filter(db.Sale.user==self._user).all()
-		money=sorted(money_in+money_out,key=lambda x:x.time)
+		transfers_in=self.session.query(db.Transfer).filter(db.Transfer.recipient==self._user).all()
+		transfers_out=self.session.query(db.Transfer).filter(db.Transfer.sender==self._user).all()
+		for m in transfers_out:
+			m.amount*=-1
+		money=sorted(money_in+money_out+transfers_in+transfers_out,key=lambda x:x.time)
 		data=[]
 		for m in money:
 			d={"amount":m.amount,"time":m.time.isoformat()}
 			if isinstance(m,db.Sale):
 				d["amount"]*=-1
 				d["Item"]=m.item.name
+			if isinstance(m,db.Transfer):
+				d["sender"]=m.sender.name
+				d["recipient"]=m.recipient.name
 			data.append(d)
 		return data
 
@@ -79,24 +89,33 @@ class matomat(object):
 		self.session.commit()
 
 	@require_auth
+	def transfer(self,amount,to):
+		amount=int(amount)
+		if amount<0:
+			raise ValueError('Can only transfer positive amounts')
+		transfer=db.Transfer(sender=self._user,recipient=to,amount=amount)
+		self.session.add(transfer)
+		self.session.commit()
+
+	@require_auth
 	def undo(self):
 		last_in=self.session.query(db.Pay).filter(db.Pay.user==self._user).order_by(db.Pay.time.desc()).first()
 		last_out=self.session.query(db.Sale).filter(db.Sale.user==self._user).order_by(db.Sale.time.desc()).first()
-		if last_in is None:
-			if last_out is None:
-				raise ValueError('Nothing to undo')
-			else:
-				self.session.delete(last_out)
-		else:
-			if last_out is None:
-				self.session.delete(last_in)
-			else:
-				self.session.delete(last_in if last_in.time>last_out.time else last_out)
+		last_transfer=self.session.query(db.Transfer).filter(db.Transfer.sender==self._user).order_by(db.Sale.time.desc()).first()
+		if last_in is None: last_in=datetime(year=1900,month=1,day=1)
+		if last_out is None: last_out=datetime(year=1900,month=1,day=1)
+		if last_transfer is None: last_transfer=datetime(year=1900,month=1,day=1)
+		to_del=max([last_in,last_out,last_transfer],key=lambda x:x.time)
+		self.session.delete(to_del)
 		self.session.commit()
 
 	def lookup_item(self,item_id):
 		item=self.session.query(db.Item).filter(db.Item.id==item_id).one()
 		return item
+
+	def lookup_user(self,user_id):
+		user=self.session.query(db.User).filter(db.User.id==user_id).one()
+		return user
 
 	def items(self):
 		items_q=self.session.query(db.Item)
